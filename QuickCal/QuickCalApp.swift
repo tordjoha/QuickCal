83197b8a17e91d6f31016ac9526024de2d118b8e
Tord Johansson <tordjoha1@gmail.com>
2025-01-27 14:47:18 +0100

    Initial Commit

---
+++ QuickCal/QuickCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCalApp.swift
+//  QuickCal
+//
+//  Created by Tord Johansson on 27/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuickCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuickCalTests/QuickCalTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCalTests.swift
+//  QuickCalTests
+//
+//  Created by Tord Johansson on 27/01/2025.
+//
+
+import Testing
+@testable import QuickCal
+
+struct QuickCalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
